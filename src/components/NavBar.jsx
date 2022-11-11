2b1baa9ca181ad7db62b5448251210022b81c228
Pedro Soares <ngrpedro22@gmail.com>
2022-11-10 21:57:22 -0300

    darkmode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,32 +3,32 @@ import logo from '../assets/logo.png'
 
 const NavBar = () => {
     return (
-        <nav className="bg-white border-b border-gray-200 px-5 sm:px-20 py-2.5 dark:bg-gray-900 shadow-lg">
+        <nav className="bg-white border-b border-gray-200 px-5 sm:px-20 py-2.5 shadow-lg">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="flex items-center w-8">
                     <img src={logo} alt="" />
-                    <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap dark:text-white font-noto">Sketch</span>
+                    <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap  font-noto">Sketch</span>
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
                         <li>
-                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">About</a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Services</a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Pricing</a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                            <a href="https://www.linkedin.com/in/pedro-nogueira399/" target={'_blank'} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Contact</a>
                         </li>
                     </ul>
                 </div>
