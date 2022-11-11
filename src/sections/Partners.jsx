a485330edb36ca0a1d8317e0a69405f701e2f08d
Pedro Soares <ngrpedro22@gmail.com>
2022-11-10 22:02:47 -0300

    parters scroll

--- src/sections/Partners.jsx
+++ src/sections/Partners.jsx
@@ -9,15 +9,16 @@ import tiktok from "../assets/partners/tiktok.svg"
 
 const Partners = () => {
     return (
-        <div className='opacity-30 flex flex-wrap items-center sm:justify-between gap-5
-                        container mx-auto px-5 sm:px-20 py-14'>
-            <img src={disney} alt="" className='w-28 h-10' />
-            <img src={duoligo} alt="" className='w-28 h-10' />
-            <img src={microsoft} alt="" className='w-28 h-10' />
-            <img src={netflix} alt="" className='w-28 h-10' />
-            <img src={nike} alt="" className='w-28 h-10' />
-            <img src={spotify} alt="" className='w-28 h-10' />
-            <img src={tiktok} alt="" className='w-28 h-10' />
+        <div className='container mx-auto px-5 sm:px-20'>
+            <div className='opacity-30 flex items-center sm:justify-between gap-10 overflow-auto py-8'>
+                <img src={disney} alt="" className='w-28 h-10' />
+                <img src={duoligo} alt="" className='w-28 h-10' />
+                <img src={microsoft} alt="" className='w-28 h-10' />
+                <img src={netflix} alt="" className='w-28 h-10' />
+                <img src={nike} alt="" className='w-28 h-10' />
+                <img src={spotify} alt="" className='w-28 h-10' />
+                <img src={tiktok} alt="" className='w-28 h-10' />
+            </div>
         </div>
     )
 }
